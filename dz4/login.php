b788c4c6106922dfe69317fe3ddaaabb9130434b
Roma <r0509133009@gmail.com>
2019-05-28 11:40:26 +0300

    fixed

--- dz4/login.php
+++ dz4/login.php
@@ -1,14 +1,17 @@
 <?php include_once('header.php'); ?>
 <?php include_once('menu.php'); ?>
 <?php
+$email = $password = '';
 if (isset($_POST['submitLogin'])) {
+    $email = trim(strip_tags($_POST['email']));
+    $password = trim(strip_tags($_POST['password']));
     $users = [];
     $file = fopen($_SERVER['DOCUMENT_ROOT'] . '/dz4/users.txt', 'r');
     while (!feof($file)) {
-        $users[] = explode('/', rtrim(fgets($file)));
+        $users[] = explode('~', rtrim(fgets($file)));
     }
     foreach ($users as $user) {
-        if (($user[0] == trim(strip_tags($_POST['email']))) && (password_verify($_POST['password'], $user[1]))) {
+        if (($user[0] == $email) && (password_verify($password, $user[1]))) {
             $_SESSION['user'] = $user[2];
             fclose($file);
             header("Location:/dz4/index.php");
@@ -23,8 +26,8 @@ if (isset($_POST['submitLogin'])) {
     <div style="width: fit-content; margin-left: 330px">
         <?php if (!empty($error)) echo $error; ?>
         <form method="post" action="login.php">
-            E-mail: <input class="textInput" type="text" name="email" placeholder="E-mail" required/><br/><br/>
-            Password: <input class="textInput" type="password" name="password" placeholder="Password"
+            E-mail: <input class="textInput" type="email" name="email" placeholder="E-mail" value="<?php if (!empty($email)) echo $email;?>" required/><br/><br/>
+            Password: <input class="textInput" type="password" name="password" placeholder="Password" minlength="8"
                              required/><br/><br/>
             <input class="submitButton" type="submit" name="submitLogin"/>
         </form>
--- dz4/registration.php
+++ dz4/registration.php
@@ -1,36 +1,39 @@
 <?php
 include_once('header.php');
 include_once('menu.php');
+$error = $name = $email = $password = '';
 if (isset($_POST['submitRegistration'])) {
     $users = [];
-    $error = '';
+    $password = trim(strip_tags($_POST['password']));
+    $name = trim(strip_tags($_POST['name']));
+    $email = trim(strip_tags($_POST['email']));
     $file = fopen($_SERVER['DOCUMENT_ROOT'] . '/dz4/users.txt', 'r');
     while (!feof($file)) {
-        $users[] = explode('/', rtrim(fgets($file)));
+        $users[] = explode('~', trim(fgets($file)));
     }
     foreach ($users as $user) {
         if ($user[0] == $_POST['email']) {
             $error = 'This E-mail is already registered';
-            break;
         }
     }
-    fclose($file);
-    $password = trim(strip_tags($_POST['email']));
-    $password = password_hash($password, PASSWORD_BCRYPT);
-    $file = fopen($_SERVER['DOCUMENT_ROOT'] . '/dz4/users.txt', 'a');
-    $userData = trim(strip_tags($_POST['email'])) . '/' . $password . '/' . $_POST['name'] . "\n";
-    fputs($file, $userData);
-    fclose($file);
-    header("Location:/dz4/login.php");
+    if (empty($error)) {
+        fclose($file);
+        $password = password_hash($password, PASSWORD_BCRYPT);
+        $file = fopen($_SERVER['DOCUMENT_ROOT'] . '/dz4/users.txt', 'a');
+        $userData = trim(strip_tags($_POST['email'])) . '~' . $password . '~' . $name . "\n";
+        fputs($file, $userData);
+        fclose($file);
+        header("Location:/dz4/login.php");
+    }
 }
 ?>
 <td id="main" style="background-color: aliceblue;">
     <div style="width: fit-content; margin-left: 330px">
         <?php if (!empty($error)) echo $error; ?>
         <form method="post" action="registration.php">
-            Name: <input class="textInput" type="text" name="name" placeholder="Name" required><br/>
-            E-mail: <input class="textInput" type="text" name="email" placeholder="E-mail" required/><br/>
-            Password: <input class="textInput" type="password" name="password" placeholder="Password" required/><br/>
+            Name: <input class="textInput" type="text" name="name" placeholder="Name" value="<?php if(!empty($name)) echo $name;?>" required><br/>
+            E-mail: <input class="textInput" type="email" name="email" placeholder="E-mail" required/><br/>
+            Password: <input class="textInput" type="password" name="password" placeholder="Password" minlength="8" required/><br/>
             <input type="submit" class="submitButton" name="submitRegistration"/>
         </form>
     </div>
